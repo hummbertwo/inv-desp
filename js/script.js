ef8623e78fa92576838c95e1ef88159bee33be4e
hummber <hummber.km@gmail.com>
2025-11-01 20:35:50 -0600

    hummber
    aomcodo de segundos y color

--- js/script.js
+++ js/script.js
@@ -69,10 +69,12 @@ function startCountdown() {
         const days = Math.floor(distance / (1000 * 60 * 60 * 24));
         const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
         document.getElementById("days").innerText = days.toString().padStart(2, "0");
         document.getElementById("hours").innerText = hours.toString().padStart(2, "0");
         document.getElementById("minutes").innerText = minutes.toString().padStart(2, "0");
+        document.getElementById("seconds").innerText = seconds.toString().padStart(2, "0");
     }, 1000);
 }
 
